130ae9e192c0a2eb1045071d252902033822ca4f
flipperbw <flipperbw@gmail.com>
2015-05-20 23:06:14 -0400

    Update twitch.user.js

--- twitch.user.js
+++ twitch.user.js
@@ -1,31 +1,35 @@
 // ==UserScript==
 // @name          Twitch Hotkeys
 // @author		  Flipperbw
-// @version       1.0
+// @version       1.0.1
 // @include       /^https?://(www|beta)\.twitch\.tv\/[a-zA-Z0-9]\w{3,24}\/[a-z]/\d+/
 // @require       http://code.jquery.com/jquery-latest.min.js
 // ==/UserScript==
 
+// Does not work for fullscreen due to browsers sucking
+
 var offset = 10;
-var k = {
+var ks = {
     "106": "j",
     "108": "l"
-}
+};
 
 //var player = $('#player object');
 var current_secs = 0;
 var player;
+var k;
 
 $(document).on("keypress", function (e) {
-    if (k[e.keyCode] == "j") {
+    k = ks[e.keyCode];
+    if (k == "j") {
         if (!player) {
             player = $('#player object')[0];
         }
         current_secs = player.getVideoTime();
-        new_secs = Math.ceil(current_secs - offset, 0);
+        new_secs = Math.max(current_secs - offset, 0.1);
         player.videoSeek(new_secs);
     }
-    else if (k[e.keyCode] == "l") {
+    else if (k == "l") {
         if (!player) {
             player = $('#player object')[0];
         }
